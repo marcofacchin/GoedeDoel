0f4f8ddf09ad632fdd2d1404f63bc838c216a297
MarcoFacchin <marco.facchin@vdabcampus.be>
2024-07-25 14:38:11 +0200

    test equals van GoedeDoel op type object

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -6,6 +6,8 @@ import be.vdab.goededoel.GoedeDoel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 class GoedeDoelTest {
 	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
@@ -33,5 +35,10 @@ class GoedeDoelTest {
 	@Test
 	void doelenMetVerschillendeNaamZijnVerschillend() {
 		assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+	}
+
+	@Test
+	void doelVrschiltVanEenObjectVanEenAnderType() {
+		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
 }
